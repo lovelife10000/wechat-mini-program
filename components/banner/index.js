0e1ba13b21d8c6f4c82598613cd162bedb4b38e6
lovelife10000 <1358180015@qq.com>
2018-04-23 15:51:20 +0800

    init

---
+++ components/banner/index.js
@@ -0,0 +1,15 @@
+const app = getApp()
+Page({
+  data: {
+    movies: [
+      { url: 'https://ss0.bdstatic.com/94oJfD_bAAcT8t7mm9GUKT-xh_/timg?image&quality=100&size=b4000_4000&sec=1524463061&di=45e706e84912644de321b8b434fc60b6&src=http://imgsrc.baidu.com/imgad/pic/item/bf096b63f6246b60553a62a0e1f81a4c510fa22a.jpg' },
+      { url: 'https://ss0.bdstatic.com/94oJfD_bAAcT8t7mm9GUKT-xh_/timg?image&quality=100&size=b4000_4000&sec=1524463061&di=45e706e84912644de321b8b434fc60b6&src=http://imgsrc.baidu.com/imgad/pic/item/bf096b63f6246b60553a62a0e1f81a4c510fa22a.jpg' },
+      { url: 'https://ss0.bdstatic.com/94oJfD_bAAcT8t7mm9GUKT-xh_/timg?image&quality=100&size=b4000_4000&sec=1524463061&di=45e706e84912644de321b8b434fc60b6&src=http://imgsrc.baidu.com/imgad/pic/item/bf096b63f6246b60553a62a0e1f81a4c510fa22a.jpg' },
+      { url: 'https://ss0.bdstatic.com/94oJfD_bAAcT8t7mm9GUKT-xh_/timg?image&quality=100&size=b4000_4000&sec=1524463061&di=45e706e84912644de321b8b434fc60b6&src=http://imgsrc.baidu.com/imgad/pic/item/bf096b63f6246b60553a62a0e1f81a4c510fa22a.jpg' }
+    ],
+ 
+  },
+
+  onLoad: function () {
+  }
+})    
